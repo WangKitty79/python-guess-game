0598f9e52704086681f6d9ba6cca2551329aad5f
wangkitty <annie12337@gmail.com>
2021-10-05 14:20:44 +0800

    主邏輯完成

---
+++ guess.game.py
@@ -0,0 +1,16 @@
+import random
+
+x = random.randint(1,50)
+print(x)
+
+for i in range(5):
+    y = int(input('請猜數字('1~50')'))
+    if x == y:
+        print('猜對了!')
+        break
+    if x>y:
+        print('猜高一點')
+    else:
+        print('猜低一點')
+        
+    print('猜錯了!')
